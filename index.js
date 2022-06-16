550245001a6f205136bc2c9832a36407e8efc186
rahimedu <edwin.duche82@gmail.com>
2022-06-16 12:42:30 -0400

    message

--- index.js
+++ index.js
@@ -1,9 +1,41 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  for (let i = 0; i < word.length / 2; i++) {
+    const j = word.length - 1 - i;
+  
+    if (word[i] !== word[j]) return false;
+  }
+
+  return true;
 }
 
 /* 
+r a c e c a r
+0 1 2 3 4 5 6
+i           j
+
+r a c e c a r
+0 1 2 3 4 5 6
+  i       j
+
+r a c e c a r
+0 1 2 3 4 5 6
+    i   j
+
+r a c e c a r
+0 1 2 3 4 5 6
+      ij
+
+
   Add your pseudocode here
+  That means if the
+  first letter is the same as last letter, and the second letter is the same
+  as the second to last letter, etc. until we reach the middle, return true.
+
+  iterate from the beginning to the middle
+    check each letter to the correponding letter from the end
+      if any letters don't match, return false
+
+  return true
 */
 
 /*
@@ -20,6 +52,11 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("abba"));
 }
 
 module.exports = isPalindrome;
